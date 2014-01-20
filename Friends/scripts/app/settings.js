4530f1e3cb9d86e0c773544c7f2159f6b5dfc5a6
Burke Holland <burkeholland@gmail.com>
2014-01-20 18:43:13 +0200

    Workspace created.

---
+++ Friends/scripts/app/settings.js
@@ -0,0 +1,40 @@
+/**
+ * Application Settings
+ */
+
+var appSettings = {
+    
+    everlive: {
+        apiKey: '1Rqr1816ukgvc9ya', // Put your Everlive API key here
+        scheme: 'http'
+    },
+    
+    eqatec: {
+        productKey: 'cc9b7aad64314a4897b738babe197b99',  // Put your EQATEC product key here
+        version: '1.0.0.0' // Put your application version here
+    },
+    
+    facebook: {
+        appId: '1408629486049918', // Put your Facebook App ID here
+        redirectUri: 'https://www.facebook.com/connect/login_success.html' // Put your Facebook Redirect URI here
+    },
+    
+    google: {
+        clientId: '406987471724-q1sorfhhcbulk6r5r317l482u9f62ti8.apps.googleusercontent.com', // Put your Google Client ID here
+        redirectUri: 'http://localhost' // Put your Google Redirect URI here
+    },
+    
+    liveId: {
+        clientId: '000000004C10D1AF', // Put your LiveID Client ID here
+        redirectUri: 'https://login.live.com/oauth20_desktop.srf' // Put your LiveID Redirect URI here
+    },
+    
+    adfs: {
+        adfsRealm: '$ADFS_REALM$', // Put your ADFS Realm here
+        adfsEndpoint: '$ADFS_ENDPOINT$' // Put your ADFS Endpoint here
+    },
+    
+    messages: {
+        removeActivityConfirm: 'Are you sure you want to delete this Activity?'
+    }
+};
